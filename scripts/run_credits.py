688e73dd68f5d901edd2d071581f27827589ace0
Xiaolan Li <xiaolan.carol.li@gmail.com>
2020-06-29 21:23:41 -0500

    restructured codebase; added scripts for cloud running, local scripts for setting up

---
+++ scripts/run_credits.py
@@ -0,0 +1,11 @@
+# aggregate method
+import credit
+from src.aggregates import *
+
+
+
+get_highest_cb_each_cat(l_cards)
+get_all_cb(l_cards)
+get_all_alerts(l_cards)
+get_latest_alerts(l_cards)
+get_all_restrictions(l_cards)
