f279e4b0378bdc387e7d50ec3b4615bca93ee259
Xiaolan Li <xiaolan.carol.li@gmail.com>
2020-07-13 10:59:55 -0500

    updated get price module to update to real price in a separate run

--- src/get_prices.py
+++ src/get_prices.py
@@ -1,5 +1,6 @@
 import json
 from yahoo_fin import stock_info as si
+from datetime import date
 
 def get_static_prices(fn=r'/Users/xiaolan/Documents/repos/FinProject/local_src/static_prices.json'):
     """
@@ -19,5 +20,21 @@ def get_live_prices(l):
     data = {}
     for i, s in enumerate(l):
         print("getting {} of {}".format(i+1, len(l)))
-        data[s.upper()] = si.get_live_price(s.lower())
+        if s!='date':
+            data[s.upper()] = si.get_live_price(s.lower())
+        else:
+            data['date'] = str(date.today())
+    return data
 
+def save_prices(data, fn=r'/Users/xiaolan/Documents/repos/FinProject/local_src/static_prices.json'):
+
+    with open(fn, 'w') as fp:
+        json.dump(data, fp)
+
+if __name__ == "__main__":
+    pre_data = get_static_prices()
+    l = list(pre_data.keys())
+    data = get_live_prices(l)
+    save_prices(data)
+    print('Done')
+    
